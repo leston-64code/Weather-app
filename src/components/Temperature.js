0c4b5d01baaae8d05b2595d78ad19359b02590e8
Leston <lestonair6574@gmail.com>
2022-07-25 21:11:01 +0530

    seacrch box css done

--- src/components/Temperature.js
+++ src/components/Temperature.js
@@ -3,8 +3,17 @@ import "./style.css"
 
 function Temperature() {
   return (
-    <div >
-   
+    <div className='one'>
+     <div className="two">
+      <div className="search-box">
+     <div className="search-box-one">
+     <input type="text" className="search-input" placeholder='Enter city'/>
+     </div>
+     <div className="search-box-two">
+      <button className='search-button'>Search</button>
+     </div>
+      </div>
+     </div>
 
     </div>
   )
