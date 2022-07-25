faf6acf0cbffe2efff5eead9f919eaa6770cd6e7
Leston <lestonair6574@gmail.com>
2022-07-25 23:04:25 +0530

    further css done

--- src/components/Temperature.js
+++ src/components/Temperature.js
@@ -22,9 +22,38 @@ function Temperature() {
       </div>
 
       <div className="three">
-        <div className="three-one">one</div>
+        <div className="three-one">
+        <i class="wi wi-day-storm-showers icon-one"></i>
+        </div>
         <div className="three-two">two</div>
-        <div className="three-three">three</div>
+        <div className="three-three">
+          <div className="three-three-flexer">
+          <div className="last-one hello ">
+            <div className="last-container">
+              <div className="onner"><i class="wi wi-night-sleet"></i></div>
+              <div className="twoer">gg</div>
+            </div>
+          </div>
+          <div className="last-two hello ">
+          <div className="last-container">
+          <div className="onner"><i class="wi wi-night-sleet"></i></div>
+              <div className="twoer">gg</div>
+          </div>
+          </div>
+          <div className="last-three hello ">
+          <div className="last-container">
+          <div className="onner"><i class="wi wi-night-sleet"></i></div>
+              <div className="twoer">gg</div>
+          </div>
+          </div>
+          <div className="last-four hello ">
+          <div className="last-container">
+          <div className="onner"><i class="wi wi-night-sleet"></i></div>
+              <div className="twoer">gg</div>
+          </div>
+          </div>
+          </div>
+        </div>
       </div>
     </div>
   );
