5cf97dedfb9d6289112cce12615c80b092a177c5
Leston <lestonair6574@gmail.com>
2022-07-25 21:15:08 +0530

    little update

--- src/components/Temperature.js
+++ src/components/Temperature.js
@@ -1,22 +1,25 @@
-import React from 'react'
-import "./style.css"
+import React from "react";
+import "./style.css";
 
 function Temperature() {
   return (
-    <div className='one'>
-     <div className="two">
-      <div className="search-box">
-     <div className="search-box-one">
-     <input type="text" className="search-input" placeholder='Enter city'/>
-     </div>
-     <div className="search-box-two">
-      <button className='search-button'>Search</button>
-     </div>
+    <div className="one">
+      <div className="two">
+        <div className="search-box">
+          <div className="search-box-one">
+            <input
+              type="text"
+              className="search-input"
+              placeholder="Enter city"
+            />
+          </div>
+          <div className="search-box-two">
+            <button className="search-button">Search</button>
+          </div>
+        </div>
       </div>
-     </div>
-
     </div>
-  )
+  );
 }
 
-export default Temperature
+export default Temperature;
