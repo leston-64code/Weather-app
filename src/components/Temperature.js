9b0957232686be673f6d88b3b6c08677edb8ba53
Leston <lestonair6574@gmail.com>
2022-07-26 10:10:30 +0530

    done all css

--- src/components/Temperature.js
+++ src/components/Temperature.js
@@ -25,31 +25,53 @@ function Temperature() {
         <div className="three-one">
         <i class="wi wi-day-storm-showers icon-one"></i>
         </div>
-        <div className="three-two">two</div>
+        <div className="three-two">
+          <div className="setter">
+            <div className="three-two-one ok">
+              <div className="super">
+                <div className="super-one">
+               <p>25.57&#8451;</p>
+                  </div>
+                <div className="super-two">
+                  <p className="child-one">CLOUDS</p><br />
+                  <p className="child-two">Tokyo,JP</p>
+                </div>
+              </div>
+            </div>
+            <div className="three-two-two ok">
+              <p className="super-child-one">26/07/22</p>
+              <p className="super-child-two">09:01 AM</p>
+            </div>
+          </div>
+        </div>
         <div className="three-three">
           <div className="three-three-flexer">
           <div className="last-one hello ">
             <div className="last-container">
-              <div className="onner"><i class="wi wi-night-sleet"></i></div>
-              <div className="twoer">gg</div>
+              <div className="onner"><i class="wi wi-sunset"></i></div>
+              <div className="twoer"><p>Sunset
+              <br />10:03 PM</p></div>
             </div>
           </div>
           <div className="last-two hello ">
           <div className="last-container">
-          <div className="onner"><i class="wi wi-night-sleet"></i></div>
-              <div className="twoer">gg</div>
+          <div className="onner"><i class="wi wi-humidity"></i></div>
+              <div className="twoer"><p>Humidity
+              <br />10:03 PM</p></div>
           </div>
           </div>
           <div className="last-three hello ">
           <div className="last-container">
-          <div className="onner"><i class="wi wi-night-sleet"></i></div>
-              <div className="twoer">gg</div>
+          <div className="onner"><i class="wi wi-fog"></i></div>
+              <div className="twoer"><p>Pressure
+              <br />10:03 PM</p></div>
           </div>
           </div>
           <div className="last-four hello ">
           <div className="last-container">
-          <div className="onner"><i class="wi wi-night-sleet"></i></div>
-              <div className="twoer">gg</div>
+          <div className="onner"><i class="wi wi-strong-wind"></i></div>
+              <div className="twoer"><p>Wind
+              <br />10:03 PM</p></div>
           </div>
           </div>
           </div>
